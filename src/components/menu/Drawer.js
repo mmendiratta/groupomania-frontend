b7915e680b3978b358cd249817ba11df54e918c0
mmendiratta <mannimendiratta@yahoo.com>
2022-05-10 19:43:06 -0500

    update drawer and readme

--- src/components/menu/Drawer.js
+++ src/components/menu/Drawer.js
@@ -5,12 +5,14 @@ import AppBar from '@mui/material/AppBar';
 import CssBaseline from '@mui/material/CssBaseline';
 import Toolbar from '@mui/material/Toolbar';
 import List from '@mui/material/List';
-import Typography from '@mui/material/Typography';
 import DeleteAccount from "../accounts/delete/DeleteAccount";
 import CreatePost from "../feed/createPost/CreatePost"
 import Logout from "../accounts/logout/Logout";
 import Feed from '../feed/Feed';
 import { ListItemIcon } from '@mui/material';
+import CreateIcon from '@mui/icons-material/Create';
+import DeleteIcon from '@mui/icons-material/Delete';
+import LogoutIcon from '@mui/icons-material/Logout';
 import icon from "../../../src/icon-left-font-monochrome-white.png"
 
 
@@ -20,7 +22,7 @@ export default function NavDrawer() {
     return (
         <Box sx={{ display: 'flex' }}>
             <CssBaseline />
-            <AppBar position="fixed" sx={{ zIndex: (theme) => theme.zIndex.drawer + 1 }}>
+            <AppBar position="fixed" sx={{ background: "rgb(15,31,65)", zIndex: (theme) => theme.zIndex.drawer + 1 }}>
                 <Toolbar>
                     <img src={icon} alt="logo" height={100} width={120} />
 
@@ -35,18 +37,21 @@ export default function NavDrawer() {
                 }}
             >
                 <Toolbar />
-                <Box sx={{ overflow: 'auto' }}>
+                <Box sx={{ overflow: 'auto', marginLeft: "12px" }}>
                     <List>
-                        <ListItemIcon key="create">
+                        <ListItemIcon key="whitespace">
                             <CreatePost />
                         </ListItemIcon>
                         <ListItemIcon key="create">
+                            <CreateIcon />
                             <CreatePost />
                         </ListItemIcon>
                         <ListItemIcon key="delete">
+                            <DeleteIcon />
                             <DeleteAccount />
                         </ListItemIcon>
                         <ListItemIcon key="logout">
+                            <LogoutIcon />
                             <Logout />
                         </ListItemIcon>
                     </List>
